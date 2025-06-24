bf951cace1a42c60703bab4e7aa7da4d0cfba15f
PhongVu0705 <pvu0705@gmail.com>
2025-06-24 22:07:34 +0700

    update code section 1. create array to store data

--- frontend/src/pages/blog/blogSection1.jsx
+++ frontend/src/pages/blog/blogSection1.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import VerticalPostItem from "./verticalPostItem";
+
 import "../../assets/style/style.css";
 import "../../assets/style/blog.css";
 import blog1 from "../../assets/images/blog/blog1.png";
@@ -8,6 +10,66 @@ import blog4 from "../../assets/images/blog/blog4.png";
 import blog5 from "../../assets/images/blog/blog5.png";
 
 const BlogSection1 = () => {
+  const blogArray = [
+    {
+      id: 0,
+      title: "HỘI SÁCH THÁI HÀ KHUÂN SÁCH VỀ NHÀ",
+      description:
+        'Chào năm cũ - đón năm mới, "BỮA TIỆC SÁCH" dành cho tất cả các bạn độc giả của chúng mình để gặp gỡ và chia sẻ những cuốn sách Hot nhất năm 2024 với những ưu đãi và nhiều quà tặng siêu hấp dẫn',
+      image: blog1,
+      link: "/tin-tuc-detail/",
+      hot: true,
+    },
+    {
+      id: 1,
+      title: "Workshop tranh in khắc cao su",
+      description:
+        "Tham gia workshop để trải nghiệm nghệ thuật in khắc cao su, tạo ra những tác phẩm độc đáo và sáng tạo.",
+      image: blog2,
+      link: "/tin-tuc-detail/",
+      hot: false,
+    },
+    {
+      id: 2,
+      title: "Giải chạy Dalat Ultra Trail 2025",
+      description:
+        "Tham gia giải chạy Dalat Ultra Trail 2025 để trải nghiệm thiên nhiên tuyệt đẹp và thử thách bản thân.",
+      image: blog3,
+      link: "/tin-tuc-detail/",
+      hot: false,
+    },
+    {
+      id: 3,
+      title: "Cà phê thứ 7 Nhà kính kiểu Joost",
+      description:
+        "Thưởng thức cà phê tại Nhà kính kiểu Joost vào thứ 7, nơi mang đến không gian thư giãn và trải nghiệm độc đáo.",
+      image: blog4,
+      link: "/tin-tuc-detail/",
+      hot: false,
+    },
+    {
+      id: 4,
+      title: "CHÙM NHO CINEMA 5: IRAN IN A FRAME Chiếu phim: 'Close-up' (1990)",
+      description:
+        "Tham gia CHÙM NHO CINEMA 5 để thưởng thức bộ phim 'Close-up' (1990) của Iran, khám phá nghệ thuật điện ảnh độc đáo.",
+      image: blog5,
+      link: "/tin-tuc-detail/",
+      hot: false,
+    },
+  ];
+  const blogArrayDom = blogArray.map((item) => {
+    if (item.hot) return null;
+    return (
+      <VerticalPostItem
+        key={item.id}
+        img={item.image}
+        title={item.title}
+        author="Bunny"
+        date="27/12/2024"
+      />
+    );
+  });
+
   return (
     <section className="blog1">
       <div className="container">
@@ -16,97 +78,21 @@ const BlogSection1 = () => {
             <div className="post__card--large">
               <a href="/tin-tuc-detail/" className="blog__link">
                 <div className="post__img blog1__main-image">
-                  <img src={blog1} alt="blog" />
+                  <img src={blogArray[0].image} alt="blog" />
                 </div>
                 <div className="post__content">
                   <h2 className="title txt-ellip txt-ellip--2">
-                    HỘI SÁCH THÁI HÀ KHUÂN SÁCH VỀ NHÀ
+                    {blogArray[0].title}
                   </h2>
                   <p className="desc txt-ellip txt-ellip--2">
-                    Chào năm cũ - đón năm mới, "BỮA TIỆC SÁCH" dành cho tất cả
-                    các bạn độc giả của chúng mình để gặp gỡ và chia sẻ những
-                    cuốn sách Hot nhất năm 2024 với những ưu đãi và nhiều quà
-                    tặng siêu hấp dẫn
+                    {blogArray[0].description}
                   </p>
                 </div>
               </a>
             </div>
           </div>
           <div className="blog1__right">
-            <div className="verticle-post">
-              <div className="verticle-post__item">
-                <a href="/tin-tuc-detail/" className="verticle-post__img">
-                  <img src={blog2} alt="blog" />
-                </a>
-                <div className="verticle-post__content">
-                  <a className="blog__link" href="/tin-tuc-detail/">
-                    <h3 className="verticle-post__title txt-ellip txt-ellip--2">
-                      Workshop tranh in khắc cao su
-                    </h3>
-                  </a>
-                  <div className="post__date">
-                    <span>
-                      <strong>Bunny,</strong> 27/12/2024
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="verticle-post__item">
-                <a href="/tin-tuc-detail/" className="verticle-post__img">
-                  <img src={blog3} alt="blog" />
-                </a>
-                <div className="verticle-post__content">
-                  <a className="blog__link" href="/tin-tuc-detail/">
-                    <h3 className="verticle-post__title txt-ellip txt-ellip--2">
-                      Giải chạy Dalat Ultra Trail 2025
-                    </h3>
-                  </a>
-                  <div className="post__date">
-                    <span>
-                      <strong>Bunny,</strong> 27/12/2024
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="verticle-post__item">
-                <a href="/tin-tuc-detail/" className="verticle-post__img">
-                  <img src={blog4} alt="blog" />
-                </a>
-                <div className="verticle-post__content">
-                  <a className="blog__link" href="/tin-tuc-detail/">
-                    <h3 className="verticle-post__title txt-ellip txt-ellip--2">
-                      Cà phê thứ 7 Nhà kính kiểu Joost
-                    </h3>
-                  </a>
-                  <div className="post__date">
-                    <span>
-                      <strong>Bunny,</strong> 27/12/2024
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="verticle-post__item">
-                <a href="/tin-tuc-detail/" className="verticle-post__img">
-                  <img src={blog5} alt="blog" />
-                </a>
-                <div className="verticle-post__content">
-                  <a className="blog__link" href="/tin-tuc-detail/">
-                    <h3 className="verticle-post__title txt-ellip txt-ellip--2">
-                      CHÙM NHO CINEMA 5: IRAN IN A FRAME Chiếu phim: "Close-up"
-                      (1990)
-                    </h3>
-                  </a>
-                  <div className="post__date">
-                    <span>
-                      <strong>Bunny,</strong> 27/12/2024
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <div className="verticle-post">{blogArrayDom}</div>
           </div>
         </div>
       </div>
