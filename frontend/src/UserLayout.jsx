4055b6c968f040093c4addba62eff2e9317780b2
AGL-NguyenHaiHoangGiang <ng_hoang_giang@allgrow-labo.jp>
2025-07-17 22:28:12 +0700

    edit Route

--- frontend/src/UserLayout.jsx
+++ frontend/src/UserLayout.jsx
@@ -1,43 +1,39 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from 'react-router-dom';
 
-import "./assets/style/style.css";
-import "./assets/style/home.css";
+import './assets/style/style.css';
+import './assets/style/home.css';
 import "./assets/style/blog.css";
-import "./assets/style/event.css";
-import "./assets/style/detail.css";
+import './assets/style/event.css';
+import './assets/style/detail.css';
 import "./assets/style/account.css";
 
-import Header from "./components/header";
-import Footer from "./components/footer";
-import Loading from "./components/loading";
+import Header from './components/header';
+import Footer from './components/footer';
+import Loading from './components/loading';
 
-import Home from "./pages/home";
-import News from "./pages/news";
+import Home from './pages/home';
+import News from './pages/news';
 import NewsCategory from "./pages/newsCategory";
 import NewDetail from "./pages/newsDetail";
-import EventCategory from "./pages/event-category";
-import EventDetail from "./pages/event-detail";
-import Events from "./pages/events";
+import EventCategory from './pages/event-category';
+import EventDetail from './pages/event-detail';
+import Events from './pages/events';
+import NotFound from './pages/404';
 import Account from "./pages/account";
-import NotFound from "./pages/404";
 
 export default function UserLayout() {
   const location = useLocation();
 
   const getMainClass = () => {
     const path = location.pathname;
-    if (path === "/") return "home";
-    else if (path === "/tin-tuc/") return "blog";
-    else if (path.startsWith("/tin-tuc/") && path.split("/").length === 3)
-      return "blog category";
-    else if (path.startsWith("/tin-tuc/") && path.split("/").length === 4)
-      return "blog blog-single";
-    else if (path.startsWith("/su-kien/") && path.split("/").length === 4)
-      return "event single";
-    else if (path.startsWith("/loai-su-kien/") || path.startsWith("/su-kien"))
-      return "page-event";
+    if (path === '/') return 'home';
+    else if (path === '/tin-tuc/') return 'blog';
+    else if (path.startsWith('/tin-tuc/') && path.split('/').length === 3) return 'blog category';
+    else if (path.startsWith('/tin-tuc/') && path.split('/').length === 4) return 'blog blog-single';
+    else if (path.startsWith('/su-kien/') && path.split('/').length === 4) return 'event single';
+    else if (path.startsWith('/loai-su-kien/') || path.startsWith('/su-kien')) return 'page-event';
     else if (path === "/tai-khoan") return "account";
-    else return "pages";
+    else return 'pages';
   };
 
   return (
@@ -49,15 +45,12 @@ export default function UserLayout() {
           <Route path="/" element={<Home />} />
           <Route path="/tin-tuc" element={<News />} />
           <Route path="/tin-tuc/:category" element={<NewsCategory />} />
-          <Route path="/tin-tuc/:category/:slug" element={<NewDetail />} />
-          <Route
-            path="/loai-su-kien/:categorySlug"
-            element={<EventCategory />}
-          />
+          <Route path="/tin-tuc/:category/:id" element={<NewDetail />} />
+          <Route path="/loai-su-kien/:categorySlug" element={<EventCategory />} />
           <Route path="/su-kien/:slug" element={<EventDetail />} />
           <Route path="/su-kien" element={<Events />} />
-          <Route path="/tai-khoan" element={<Account />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/tai-khoan" element={<Account />} />
         </Routes>
       </main>
       <Footer />
