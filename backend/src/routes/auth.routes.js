528f9958fae7041e56c00cb4870d42251f37e5ef
devboy2011 <buituananhit.contact@gmail.com>
2025-07-19 21:49:05 +0700

    add auth: apply access token, refresh token to routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,5 +5,9 @@ const authController = require('../controllers/auth.controller');
 // POST /api/v0/signup
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+
+router.post('/refresh-token', authController.refreshToken);
+router.post('/verify-token', authController.verifyToken);
 
 module.exports = router;
