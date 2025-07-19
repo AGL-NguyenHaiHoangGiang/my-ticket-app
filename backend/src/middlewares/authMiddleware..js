89b0b60ff9e89cf3c74bc0d1692314ea9617498a
devboy2011 <buituananhit.contact@gmail.com>
2025-07-19 17:15:42 +0700

    add auth middleware, authRoles middleware

---
+++ backend/src/middlewares/authMiddleware..js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    } 
+
+    const token = req.headers['authorization']?.split(' ')[1];
+    
+    if (!token) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+    
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+        if (err) {
+        return res.status(401).json({ error: 'Failed to authenticate token' });
+        }
+    
+        req.user = decoded;
+        next();
+    });
+}
+
+module.exports = authMiddleware;
---
+++ backend/src/middlewares/authRolesMiddleware.js
@@ -0,0 +1,16 @@
+const authorizeRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user || !req.user.role) {
+        return res.status(403).json({ message: 'Access denied. No user role found.' });
+        }
+    
+        const userRole = req.user.role;
+        if (!allowedRoles.includes(userRole)) {
+        return res.status(403).json({ message: 'Access denied. You do not have the required role.' });
+        }
+    
+        next();
+    };
+}
+
+module.exports = authorizeRoles;
